34c109fa2632ff27df58a84d8cc30997abf1be81
LucasRdSilva <lucasrsilva42@gmail.com>
2021-09-11 18:43:22 -0300

    Pagination

---
+++ backend/src/main/java/com/lucasrds/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.lucasrds.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucasrds.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
