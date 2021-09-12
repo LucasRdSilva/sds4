42965024c9510633839a847427d2e655eda10eec
LucasRdSilva <lucasrsilva42@gmail.com>
2021-09-12 02:30:06 -0300

    Ajuste SaleSuccess

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
-import { SaleSucess } from 'types/sale';
+import { SaleSuccess } from 'types/sale';
+
 
 import { round } from 'utils/format';
 import { BASE_URL } from 'utils/requests'
@@ -35,7 +36,7 @@ const BarChart = () => {
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/success-by-seller`)
             .then(response => {
-                const data = response.data as SaleSucess[];
+                const data = response.data as SaleSuccess[];
                 const myLabels = data.map(x => x.sellerName);
                 const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
 
